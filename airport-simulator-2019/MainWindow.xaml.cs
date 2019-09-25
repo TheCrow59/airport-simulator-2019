846622ce26aa0abb4dd7c22ae019f5babd59a311
Evgeniy934 <borovkov.evg@gmail.com>
2019-09-25 02:33:06 -0700

    main window with buttons

---
+++ airport-simulator-2019/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace airport_simulator_2019
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RealTime_Click(object sender, RoutedEventArgs e)
+        {
+            RealTime.IsEnabled = false;
+            Fast.IsEnabled = true;
+            VeryFast.IsEnabled = true;
+        }
+
+        private void Fast_Click(object sender, RoutedEventArgs e)
+        {
+            RealTime.IsEnabled = true;
+            Fast.IsEnabled = false;
+            VeryFast.IsEnabled = true;
+        }
+
+        private void VeryFast_Click(object sender, RoutedEventArgs e)
+        {
+            RealTime.IsEnabled = true;
+            Fast.IsEnabled = true;
+            VeryFast.IsEnabled = false;
+        }
+    }
+}
